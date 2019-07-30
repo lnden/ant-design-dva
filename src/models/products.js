d0dff071653a55aa14a58c15bf5654724d05dee2
lnden <258231674@qq.com>
2019-07-30 16:02:49 +0800

    add product page

---
+++ src/models/products.js
@@ -0,0 +1,14 @@
+export default {
+    namespace: 'products',
+    state: [
+        // {name: 'dva', id: 1},
+        // {name: 'antd', id: 2},
+    ],
+    subscriptions: {},
+    reducers: {
+        'delete'(state, {payload: id}) {
+            return state.filter(item => item.id !== id)
+        }
+    },
+    effects: {}
+}
