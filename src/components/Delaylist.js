0b8a323575bd9edcf8a9b5442baf088e8685e190
lnden <258231674@qq.com>
2019-07-31 14:00:39 +0800

    add styles

--- src/components/Delaylist.js
+++ src/components/Delaylist.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import {Button} from 'antd'
+import styles from './delaylist.less'
 
 const Delaylist = ({onDelete, delaylist}) => {
     return (
         <ul>
             {
                 delaylist.map(item => {
-                    return <li key={item.id}>{item.name} <Button onClick={() => onDelete(item.id)}>删除</Button></li>
+                    return <li className={styles.list} key={item.id}>{item.name} <Button onClick={() => onDelete(item.id)}>删除</Button></li>
                 })
             }
 
