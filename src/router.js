bd973edf506a868b56812dd65563cd8acfddd32c
lnden <258231674@qq.com>
2019-07-31 13:51:23 +0800

    change layout

--- src/router.js
+++ src/router.js
@@ -1,18 +1,26 @@
 import React from 'react';
 import {Router, Route, Switch} from 'dva/router';
+import {Button} from 'antd'
 import IndexPage from './routes/IndexPage';
 import Products from './routes/Products';
 import Delaylist from './routes/delayList'
 
 function RouterConfig({history}) {
+    function handleClick(){
+        history.push('/')
+    }
     return (
-        <Router history={history}>
-            <Switch>
-                <Route path="/" exact component={IndexPage}/>
-                <Route path="/products" component={Products} />
-                <Route path="/delaylist" component={Delaylist} />
-            </Switch>
-        </Router>
+        <section>
+            <p style={{padding: '10px'}}><Button onClick={handleClick}>返回首页</Button></p>
+            <Router history={history}>
+                <Switch>
+                    <Route path="/" exact component={IndexPage}/>
+                    <Route path="/products" component={Products}/>
+                    <Route path="/delaylist" component={Delaylist}/>
+                </Switch>
+            </Router>
+        </section>
+
     );
 }
 
--- src/routes/IndexPage.js
+++ src/routes/IndexPage.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import { connect } from 'dva';
+import { Link } from 'dva/router'
+import { Button } from 'antd'
 import styles from './IndexPage.css';
 
 function IndexPage() {
     return (
         <div className={styles.normal}>
+            <ul className={styles.list}>
+                <li><Button><Link to="/products">跳转</Link></Button>dva官方示例/products</li>
+                <li><Button><Link to="/delaylist">跳转</Link></Button>test示例/delaylist</li>
+            </ul>
             <h1 className={styles.title}>Yay! Welcome to dva!</h1>
             <div className={styles.welcome}/>
-            <ul className={styles.list}>
-                <li>To get started, edit <code>src/index.js</code> and save to reload.</li>
-                <li><a href="https://github.com/dvajs/dva-docs/blob/master/v1/en-us/getting-started.md">Getting
-                    Started</a></li>
-            </ul>
         </div>
     );
 }
