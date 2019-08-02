4a6c808720a56fa79850971a8a411bf9786ac032
lnden <258231674@qq.com>
2019-08-02 18:28:48 +0800

    add all and race

--- src/models/delayList.js
+++ src/models/delayList.js
@@ -1,6 +1,8 @@
 function delay(timeout) {
     return new Promise(resolve => {
-        setTimeout(resolve, timeout);
+        setTimeout(()=>{
+            return resolve(1)
+        }, timeout);
     });
 }
 
@@ -20,18 +22,32 @@ export default {
         },
         realdelete(state, action) {
             console.log('3.最后执行Deleted')
-            return state.filter(item=>item.id!=action.payload)
+            return state.filter(item => item.id !== action.payload)
         }
     },
     effects: {
-        * delete({payload}, {call, put, select}) {
+        * delete({payload}, {call, put, select,all,race}) {
             console.log('2.delete异步执行~')
             yield call(delay, 1000);
             yield put({
-                type:'realdelete',
-                payload:payload
+                type: 'realdelete',
+                payload: payload
             })
             console.log('4.异步延迟N秒之后执行')
+
+            const otherState = yield select()
+            console.log(otherState, '获取其他model里面的state')
+
+            const [result1, result2] = yield all([
+                call(delay, 1000),
+                call(delay, 2000)
+            ])
+            console.log(result1,result2)
+            const [result3, result4] = yield race([
+                call(delay, 2000),
+                call(delay, 1000)
+            ])
+            console.log(result3,result4)
         }
     }
 }
