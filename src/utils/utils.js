7017943472eb44e2874e1bf66bf531af88b8b534
lnden <258231674@qq.com>
2019-08-14 15:36:19 +0800

    add utils

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -5,25 +5,74 @@ import {isObject} from "./verify";
  * 对象转换字符串
  *
  * @name objTostr
- * @param {name:'lily',age:18}
+ * @param objTostr({name:'lily',age:18})
  * @return name=lily&age=18
  *
  */
 export const objTostr = obj => {
-    if (!isObject(obj)) return
+    if (!isObject(obj)) return '';
 
     return Object.getOwnPropertyNames(obj)
         .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(obj[key])}`)
-        .json('&')
+        .join('&');
 }
+
+/**
+ * 字符串转对象[类似query-string]
+ *
+ * @param strToobj
+ * @param strToobj('name=lily&age=18')
+ * @return {name:'lily',age:18}
+ */
+export const query = str => {
+    // Creator an object with no prototype
+    // https://github.com/sindresorhus/query-string/issues/47
+    const ret = Object.create(null);
+
+    if (typeof str !== 'string') {
+        return ret;
+    }
+
+    // eslint-disable-next-line no-param-reassign
+    str = str.trim().replace(/^(\?|#|&)/, '');
+
+    if (!str) {
+        return ret;
+    }
+
+    str.split('&').forEach(param => {
+        const parts = param.replace(/\+/g, ' ').split('=');
+        // Firefox (pre 40) decodes `%3D` to `=`
+        // https://github.com/sindresorhus/query-string/pull/37
+        let key = parts.shift();
+        let val = parts.length > 0 ? parts.join('=') : undefined;
+
+        key = decodeURIComponent(key);
+
+        // missing `=` should be `null`:
+        // http://w3.org/TR/2012/WD-url-20120524/#collect-url-parameters
+        val = val === undefined ? null : decodeURIComponent(val);
+
+        if (ret[key] === undefined) {
+            ret[key] = val;
+        } else if (Array.isArray(ret[key])) {
+            ret[key].push(val);
+        } else {
+            ret[key] = [ret[key], val];
+        }
+    });
+
+    return ret;
+};
 
 /**
  * 创建标准的柯里化 action
  *
- * @param type
+ * @param objTostr
  * @return {function(*): {type: *, payload: *}}
  */
 export const createAction = type => payload => ({
     type,
     payload,
 });
+
