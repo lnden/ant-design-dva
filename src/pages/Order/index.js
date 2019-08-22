07c14de8cbdeecbe39767ffdab813c042a7c1b14
lnden <258231674@qq.com>
2019-08-23 00:12:02 +0800

    add content

--- src/pages/Order/index.js
+++ src/pages/Order/index.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
-import { Card,Button,Table } from 'antd'
+import { Card, Button, Table, DatePicker, Form, Select } from 'antd'
 
+const FormItem = Form.Item
+const Option = Select.Option
 export default class Order extends Component {
 
-    state={}
-    componentDidMount(){
+    state = {}
+    componentDidMount() {
         this.requestList()
     }
 
@@ -15,48 +17,48 @@ export default class Order extends Component {
     render() {
         const columns = [
             {
-                title:'订单编号',
-                dataIndex:'order_sn'
+                title: '订单编号',
+                dataIndex: 'order_sn'
             },
             {
-                title:'车辆编号',
-                dataIndex:'bike_sn'
+                title: '车辆编号',
+                dataIndex: 'bike_sn'
             },
             {
-                title:'用户名',
-                dataIndex:'user_name'
+                title: '用户名',
+                dataIndex: 'user_name'
             },
             {
-                title:'手机号',
-                dataIndex:'mobile'
+                title: '手机号',
+                dataIndex: 'mobile'
             },
             {
-                title:'里程',
-                dataIndex:'distance'
+                title: '里程',
+                dataIndex: 'distance'
             },
             {
-                title:'行驶时长',
-                dataIndex:'total_time'
+                title: '行驶时长',
+                dataIndex: 'total_time'
             },
             {
-                title:'状态',
-                dataIndex:'status'
+                title: '状态',
+                dataIndex: 'status'
             },
             {
-                title:'开始时间',
-                dataIndex:'start_time'
+                title: '开始时间',
+                dataIndex: 'start_time'
             },
             {
-                title:'结束时间',
-                dataIndex:'end_time'
+                title: '结束时间',
+                dataIndex: 'end_time'
             },
             {
-                title:'订单金额',
-                dataIndex:'total_fee'
+                title: '订单金额',
+                dataIndex: 'total_fee'
             },
             {
-                title:'实付金额',
-                dataIndex:'user_pay'
+                title: '实付金额',
+                dataIndex: 'user_pay'
             }
         ]
         return (
@@ -64,7 +66,7 @@ export default class Order extends Component {
                 <Card>
                     <FilterForm />
                 </Card>
-                <Card style={{marginTop:10}}>
+                <Card style={{ marginTop: 10 }}>
                     <Button>订单详情</Button>
                     <Button>结束订单</Button>
                 </Card>
@@ -72,8 +74,8 @@ export default class Order extends Component {
                     <Table
                         borderd
                         columns={columns}
-                        dataSource={this.state.list}
-                        pagination={this.StaticRange.pagination}
+                        // dataSource={this.state.list}
+                        // pagination={this.StaticRange.pagination}
                     />
                 </div>
             </div>
@@ -83,11 +85,58 @@ export default class Order extends Component {
 
 
 class FilterForm extends Component {
-    render(){
+    render() {
+        const { getFieldDecorator } = this.props.form;
         return (
-            <div>
-                测试
-            </div>
+            <Form layout="inline">
+                 <FormItem label="城市">
+                    {
+                        getFieldDecorator('city_id')(
+                            <Select
+                                style={{ width: 100 }}
+                                palceholder="全部"
+                            >
+                                <Option value="">全部</Option>
+                                <Option value="1">北京</Option>
+                                <Option value="2">上海</Option>
+                            </Select>
+                        )
+                    }
+                </FormItem>
+                <FormItem label="订单时间">
+                    {
+                        getFieldDecorator('start_time')(
+                            <DatePicker showTime format="YYYY-MM-DD HH:mm:ss" />
+                        )
+                    }
+                </FormItem>
+                <FormItem label="~">
+                    {
+                        getFieldDecorator('end_time')(
+                            <DatePicker showTime format="YYYY-MM-DD HH:mm:ss" />
+                        )
+                    }
+                </FormItem>
+                <FormItem label="订单状态">
+                    {
+                        getFieldDecorator('op_mode')(
+                            <Select
+                                style={{ width: 80 }}
+                                palceholder="全部"
+                            >
+                                <Option value="">全部</Option>
+                                <Option value="1">进行中</Option>
+                                <Option value="2">结束形成</Option>
+                            </Select>
+                        )
+                    }
+                </FormItem>
+                <FormItem>
+                    <Button type="primary" style={{ margin: '0 20px' }}>查询</Button>
+                    <Button>重置</Button>
+                </FormItem>
+            </Form>
         )
     }
 }
+FilterForm = Form.create({})(FilterForm)
